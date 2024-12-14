38484d198f9903a927eb55d068c9b8e6922dc76e
Secand <Boevladik@gmail.com>
2024-12-14 17:16:38 +0300

    add selection

--- servises/frontend/src/views/GameSelection.vue
+++ servises/frontend/src/views/GameSelection.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    {{ gamesList.name }}
-  </div>
+  <button class="new-game__button">Добавить игру</button>
+
+  <section>
+    <div class="card">
+      <span>
+        <button class="card__edit">✏️</button>
+      </span>
+      <h3 class="card__title">
+        {{ gamesList.name }}
+      </h3>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -11,4 +20,32 @@ const gamesList = ref(JSON.parse(localStorage.getItem("games") || ""));
 console.log(gamesList.value);
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+
+  padding: 10px;
+
+  max-width: 200px;
+  min-height: 200px;
+
+  border: solid 1px black;
+  border-radius: 10px;
+}
+
+.card__edit {
+  display: flex;
+  margin-left: auto;
+}
+
+.card__title {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.new-game__button {
+  width: 100%;
+  padding: 10px 0;
+}
+</style>
